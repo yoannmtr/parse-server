2045e51285fc36c54c10886b06ea5f7548b18d13
yoannmtr <yoann.mtr@gmail.com>
2016-07-22 11:16:52 +0200

    Add support for javascript and rest api keys

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  javascriptKey: process.env.JAVASCRIPT_KEY || '',
+  restAPIKey: process.env.REST_KEY || '',
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
